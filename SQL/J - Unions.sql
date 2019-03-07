790e60a71719f4a3474103ec2b31e9a4dafc234f
Kevin Tran <kevintraan@live.com>
2019-03-07 14:22:35 -0700

    .

--- SQL/J - Unions.sql
+++ SQL/J - Unions.sql
@@ -59,3 +59,8 @@ AS
     FROM    Staff S
         INNER JOIN Position P ON S.PositionID = P.PositionID
 GO
+
+--2. Create a list of course ID's and the number of students in the course and 
+--   UNION that with a list of the courseID and the MaxStudents of the course. 
+--   For the first list, the columns should be 'Course', 'Count', 'Type' with the type for the
+--   first list being 'actual' and the type for the second list being 'planned'
